7c26048d5296a340a2b7c962c440b7d1ab4e0dee
Wesed <dev.wfeduardo@gmail.com>
2023-09-25 10:54:34 -0300

    estruturando o design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
+  content: ['./src/**/*.tsx'],
   plugins: [],
+  theme: {
+    colors: {
+      white: '#fff',
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e'
+    },
+    fontFamily: {
+      sans: 'Roboto',
+    },
+  }
 }
 export default config
