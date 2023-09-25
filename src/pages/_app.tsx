0db770d0d34a5bf8a46734297917753c5d53bc2f
Wesed <dev.wfeduardo@gmail.com>
2023-09-25 18:03:52 -0300

    estrutura visual - criando carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import logo from '@/assets/logo.svg'
 import Image from 'next/image'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <div className="bg-gray900 text-gray100 flex flex-col items-start justify-center min-h-screen">
-    <header className="py-8 px-0 w-full max-w-widthProject my-0 mx-auto">
-      <Image src={logo} alt="" />
-    </header>
-    <Component {...pageProps} />
-  </div>
+  return (
+    <div className="flex min-h-screen flex-col items-start justify-center bg-gray900 text-gray100">
+      <header className="my-0 ml-auto w-full max-w-widthCarousel px-0 py-8">
+        <Image src={logo} alt="" />
+      </header>
+      <Component {...pageProps} />
+    </div>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,29 @@ import shirt1 from '@/assets/shirt1.png'
 import shirt2 from '@/assets/shirt2.png'
 import shirt3 from '@/assets/shirt3.png'
 import shirt4 from '@/assets/shirt4.png'
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
 import { twMerge } from 'tailwind-merge'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <div className="ml-auto flex min-h-[656px] w-full max-w-widthCarousel gap-12">
+    <div
+      ref={sliderRef}
+      className="keen-slider ml-auto flex min-h-[656px] w-full max-w-widthCarousel"
+    >
       <a
         href=""
         className={twMerge(
+          'keen-slider__slide',
           'group relative flex items-center justify-center',
-          'cursor-pointer overflow-hidden rounded-lg border bg-gradient p-1',
+          'cursor-pointer overflow-hidden rounded-lg border bg-gradient',
         )}
       >
         <Image
@@ -32,19 +45,20 @@ export default function Home() {
           )}
         >
           <strong className="">Camiseta X</strong>
-          <span className="">R$ 79,90</span>
+          <span className="text-xl font-bold text-green300">R$ 79,90</span>
         </footer>
       </a>
 
       <a
         href=""
         className={twMerge(
+          'keen-slider__slide',
           'group relative flex items-center justify-center',
-          'cursor-pointer overflow-hidden rounded-lg border bg-gradient p-1',
+          'cursor-pointer overflow-hidden rounded-lg border bg-gradient',
         )}
       >
         <Image
-          src={shirt1}
+          src={shirt2}
           alt=""
           width={520}
           height={4800}
@@ -59,7 +73,64 @@ export default function Home() {
             'opacity-100 group-hover:translate-y-0',
           )}
         >
-          {' '}
+          <strong className="text-lg">Camiseta X</strong>
+          <span className="text-xl font-bold text-green300">R$ 79,90</span>
+        </footer>
+      </a>
+
+      <a
+        href=""
+        className={twMerge(
+          'keen-slider__slide',
+          'group relative flex items-center justify-center',
+          'cursor-pointer overflow-hidden rounded-lg border bg-gradient',
+        )}
+      >
+        <Image
+          src={shirt4}
+          alt=""
+          width={520}
+          height={4800}
+          className="object-cover"
+        />
+        <footer
+          className={twMerge(
+            'flex items-center justify-between',
+            'absolute bottom-1 left-1 right-1 translate-y-[105%]',
+            'rounded-md bg-black/60 p-8 opacity-0',
+            'transition-all duration-200 ease-in-out',
+            'opacity-100 group-hover:translate-y-0',
+          )}
+        >
+          <strong className="text-lg">Camiseta X</strong>
+          <span className="text-xl font-bold text-green300">R$ 79,90</span>
+        </footer>
+      </a>
+
+      <a
+        href=""
+        className={twMerge(
+          'keen-slider__slide',
+          'group relative flex items-center justify-center',
+          'cursor-pointer overflow-hidden rounded-lg border bg-gradient',
+        )}
+      >
+        <Image
+          src={shirt3}
+          alt=""
+          width={520}
+          height={4800}
+          className="object-cover"
+        />
+        <footer
+          className={twMerge(
+            'flex items-center justify-between',
+            'absolute bottom-1 left-1 right-1 translate-y-[105%]',
+            'rounded-md bg-black/60 p-8 opacity-0',
+            'transition-all duration-200 ease-in-out',
+            'opacity-100 group-hover:translate-y-0',
+          )}
+        >
           <strong className="text-lg">Camiseta X</strong>
           <span className="text-xl font-bold text-green300">R$ 79,90</span>
         </footer>
