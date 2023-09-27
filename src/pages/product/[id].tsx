f5ed2ee5ddf5f75f95c844eabafc1ab81962f824
Wesed <dev.wfeduardo@gmail.com>
2023-09-27 19:56:08 -0300

    aplicando fall back SSG na pagina de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,10 @@
-import { GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { stripe } from '@/lib/stripe'
 import Stripe from 'stripe'
 import { twMerge } from 'tailwind-merge'
 import Image from 'next/image'
+import loader from '@/assets/loader.svg'
+import { useRouter } from 'next/router'
 
 interface ProductProps {
   product: {
@@ -15,19 +17,42 @@ interface ProductProps {
 }
 
 export default function product({ product }: ProductProps) {
+  /* eslint-disable react-hooks/rules-of-hooks */
+  const { isFallback } = useRouter()
+
   return (
     <main className="mx-auto my-0 grid max-w-widthProject grid-cols-2 items-stretch gap-16">
-      {/* img container */}
       <div className="flex h-[656px] w-full max-w-[576px] items-center justify-center rounded-lg bg-gradient p-1">
-        <Image src={product.imageUrl} alt="" width={520} height={480} />
+        {isFallback ? (
+          <div className="flex h-[656px] w-[586px] items-center justify-center">
+            <Image
+              className="animate-spin"
+              src={loader}
+              alt=""
+              width={40}
+              height={40}
+            />
+          </div>
+        ) : (
+          <Image src={product.imageUrl} alt="" width={520} height={480} />
+        )}
       </div>
-      {/* product details */}
       <div className="flex flex-col ">
-        <h1 className="text-3xl text-gray300">{product.name}</h1>
-        <span className="mt-4 block text-3xl text-green300">
-          {product.price}
-        </span>
-        <p className="mt-10 text-lg text-gray300">{product.description}</p>
+        {isFallback ? (
+          <div className="flex w-full animate-pulse flex-col gap-8">
+            <span className="h-8 bg-gray800" />
+            <span className="h-8 bg-gray800" />
+            <span className="h-20 bg-gray800" />
+          </div>
+        ) : (
+          <div>
+            <h1 className="text-3xl text-gray300">{product.name}</h1>
+            <span className="mt-4 block text-3xl text-green300">
+              {product.price}
+            </span>
+            <p className="mt-10 text-lg text-gray300">{product.description}</p>
+          </div>
+        )}
 
         <button
           className={twMerge(
@@ -42,6 +67,16 @@ export default function product({ product }: ProductProps) {
       </div>
     </main>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    /* os ids de prods colocados no path, sao carregados diretamente,
+      sem buscas na API
+    */
+    paths: [],
+    fallback: true,
+  }
 }
 
 // SSG
@@ -72,6 +107,6 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
         description: product.description,
       },
     },
-    revalidate: 60 * 60 * 2, // atualiza o conteúdo a cd 2hrs
+    revalidate: 60 * 60 * 1, // atualiza o conteúdo a cd 2hrs
   }
 }
