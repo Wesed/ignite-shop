48e74c9e50306374bb306316ecf1f4335a103a61
Wesed <dev.wfeduardo@gmail.com>
2023-09-28 15:28:13 -0300

    recebendo dados da compra e setando na tela de success

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,11 +7,11 @@ import Link from 'next/link'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex min-h-screen flex-col items-start justify-center bg-gray900 text-gray100">
-      <header className="my-0 ml-auto w-full max-w-widthCarousel px-0 py-8">
+      {/* <header className="my-0 ml-auto w-full max-w-widthCarousel px-0 py-8">
         <Link href="/">
           <Image src={logo} alt="" priority />
         </Link>
-      </header>
+      </header> */}
       <Component {...pageProps} />
     </div>
   )
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,20 +1,35 @@
+import { stripe } from '@/lib/stripe'
+import { GetServerSideProps } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
+import Stripe from 'stripe'
 import { twMerge } from 'tailwind-merge'
 
-export default function Success() {
+interface SuccessProps {
+  customerName: string
+  product: {
+    name: string
+    imageUrl: string
+  }
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <main className="mx-auto my-0 flex h-[656px] flex-col items-center justify-center">
       <h1 className="text-3xl font-bold text-gray100">Compra efetuada!</h1>
       <div className="mb-8 mt-16 flex h-36 w-full max-w-[130px] items-center justify-center rounded-lg bg-gradient p-1">
-        <Image src="" alt="" objectFit="cover" />
+        <Image
+          src={product.imageUrl}
+          width={120}
+          height={110}
+          alt=""
+          objectFit="cover"
+        />
       </div>
       <p className="max-w-[560px] text-center text-2xl text-gray300">
-        Uhuul <strong className="text-gray100">Matheus</strong>, sua {''}
-        <strong className="text-gray100">
-          Camiseta LongLine Limited Edition
-        </strong>{' '}
-        já está a caminho da sua casa.
+        Uhuul <strong className="text-gray100">{customerName}</strong>, sua {''}
+        <strong className="text-gray100">{product.name}</strong> já está a
+        caminho da sua casa.
       </p>
       <Link
         href="/"
@@ -28,4 +43,27 @@ export default function Success() {
       </Link>
     </main>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id)
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const customerName = session.customer_details!.name
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const product = session!.line_items!.data[0]!.price!.product as Stripe.Product
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  }
 }
