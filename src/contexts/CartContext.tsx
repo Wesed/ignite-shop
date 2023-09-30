57adac20f919c296360b9e23d6bcad42731aa5e5
Wesed <dev.wfeduardo@gmail.com>
2023-09-30 10:36:24 -0300

    corrigir bug localStorage no next

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useState } from 'react'
+import { ReactNode, createContext, useEffect, useState } from 'react'
 
 interface Product {
   id: string
@@ -21,10 +21,23 @@ interface CartContextProviderProps {
 }
 
 export function CartContextProvider({ children }: CartContextProviderProps) {
-  const [products, setProducts] = useState<Product[]>([])
+  const [products, setProducts] = useState<Product[]>(() => {
+    // const productsStateAsJSON = localStorage.getItem('@dev-shop:products')
+
+    // if (productsStateAsJSON) {
+    //   return JSON.parse(productsStateAsJSON)
+    // }
+
+    return []
+  })
+
+  useEffect(() => {
+    const productsJSON = JSON.stringify(products)
+    localStorage.setItem('@dev-shop:products', productsJSON)
+  }, [products])
 
   const AddNewProduct = (data: Product) => {
-    setProducts((state) => [...products, data])
+    setProducts((state) => [...state, data])
   }
 
   return (
